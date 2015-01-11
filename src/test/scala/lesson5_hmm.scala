cfcfa5362754ab6c5384fb3add2edeb97fd1966c
kensuke-mi <kensuke-mi@is.naist.jp>
2015-01-11 20:27:45 +0900

    [src/test/scala] Hmmのviterbi forwardの途中まで書いているところ

--- src/test/scala/lesson5_hmm.scala
+++ src/test/scala/lesson5_hmm.scala
@@ -7,7 +7,6 @@ import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
-
 class TrainTransitionEmissionProbs(PathToTrainFile: String,
                                    PathToModelProbMap: String){
   /*
@@ -121,15 +120,110 @@ class TrainTransitionEmissionProbs(PathToTrainFile: String,
 
 object lesson5_hmm {
 
+  def LoadTestFile(path_to_train_file: String):
+  mutable.ArrayBuffer[List[String]] = {
+    /*
+    TODO 後で前処理用のパッケージを作ってまとめて突っ込んでおくこと
+     */
+    val arraybuffer_unigram_training = mutable.ArrayBuffer[List[String]]()
 
-  def main(args: Array[String]): Unit = {
+    val source_file_ins = Source.fromFile(path_to_train_file)
+    source_file_ins.getLines().foreach({
+      line =>
+        val array_word_in_sentence = line.stripSuffix("\n").split(" ")
+        val list_word_in_setence = array_word_in_sentence.toList
+        // arrayの先頭に<s>, arrayの最後に</s>を挿入する
+        val list_of_sentence = "<s>" +: list_word_in_setence :+ "</s>"
 
+        arraybuffer_unigram_training += list_of_sentence
+    })
+    return arraybuffer_unigram_training
+  }
+
+
+  def LoadHmmModel(PathToModelProbMap: String):
+  Tuple3[mutable.Map[String, Float], mutable.Map[String, Float], mutable.Map[String, Int]] = {
+    /*
+    モデルファイルを読み込む
+    モデルファイルはtransition probとemission probの２つにわけて返す
+     */
+    val MapTransitionProb = mutable.Map[String, Float]()
+    val MapEmissionProb = mutable.Map[String, Float]()
+    val MapPossibleTags = mutable.Map[String, Int]()
+
+    val InsTransitionMapIO =
+      new MODEL_IO(path_to_file = PathToModelProbMap, separator = "\t")
+    val MapHmmModel = InsTransitionMapIO.load_model()
+    MapHmmModel.foreach({
+      case (key, value) =>
+        val keyString = key.toString
+        val valueFloat = value.toString.toFloat
+        val TypeTag = keyString.split(" ")(0)
+        val PosTag = keyString.split(" ")(1)
+        MapPossibleTags(PosTag) = 1
+
+        val ContextWord: String = keyString.split(" ").slice(1,3).mkString(" ") // 返すmapのキーを作成する
+        if(TypeTag=="T") MapTransitionProb(ContextWord) = valueFloat
+        else if(TypeTag=="E") MapEmissionProb(ContextWord) = valueFloat
+    })
+    return Tuple3(MapTransitionProb, MapEmissionProb, MapPossibleTags)
+  }
+
+
+  def HmmPosPredicting(ArrayBufferTestFile: mutable.ArrayBuffer[List[String]],
+                       MapTransitionProb: mutable.Map[String, Float],
+                       MapEmissionProb: mutable.Map[String, Float],
+                       MapPossibleTags: mutable.Map[String, Int]) = {
+
+    HmmViterbiForward(ArrayBufferTestFile, MapTransitionProb, MapEmissionProb, MapPossibleTags)
+    // HmmViterbiBackward
+  }
+
+
+  def HmmViterbiForward(ArrayBufferTestFile: mutable.ArrayBuffer[List[String]],
+                        MapTransitionProb: mutable.Map[String, Float],
+                        MapEmissionProb: mutable.Map[String, Float],
+                        MapPossibleTags: mutable.Map[String, Int]) = {
+
+    val MapBestScore = mutable.Map[String, Float]()
+    val MapBestEdge = mutable.Map[String, String]()
+
+    val StartTag = "0 <s>"
+    MapBestScore(StartTag) = 0f
+    MapBestEdge(StartTag) = "NULL"
+
+    // TODO forward stepの続きを書いていくことq
+  }
+
+
+  def HmmViterbiBackward() = {
+
+  }
+  // ------------------------------------------------
+
+
+  def TrainModel():Unit = {
     val PathToTrainFile: String = "src/test/resources/test/05-train-input.txt"
     val PathToModelProbMap: String = "src/test/scala/hmm_model_prob.tsv"
     val InsTrainProbs = new TrainTransitionEmissionProbs(
       PathToTrainFile,
       PathToModelProbMap)
     InsTrainProbs.TrainMain()
+  }
+
+  def main(args: Array[String]): Unit = {
+    //TrainModel()
+
+    val PathToModelProbMap: String = "src/test/scala/hmm_model_prob.tsv"
+    val PathToTestFile: String = "src/test/resources/test/05-test-input.txt"
+    val ArrayBufferTestFile = LoadTestFile(PathToTestFile)
+
+    val TupleModelMaps = LoadHmmModel(PathToModelProbMap)
+    val MapTransitionProb = TupleModelMaps._1
+    val MapEmissionProb = TupleModelMaps._2
+    val MapPossibleTags = TupleModelMaps._3
+    HmmPosPredicting(ArrayBufferTestFile, MapTransitionProb, MapEmissionProb, MapPossibleTags)
+
   }
 
 }
